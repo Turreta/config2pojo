be39d5eb1ea50d88f04ada158d2c153e62ac72e1
Turreta.com <Turreta@users.noreply.github.com>
2017-01-06 18:58:24 +0800

    Update unit test

--- src/test/java/com/turreta/config2pojo/ComTurretaConfig2pojoApplicationTests.java
+++ src/test/java/com/turreta/config2pojo/ComTurretaConfig2pojoApplicationTests.java
@@ -1,16 +1,21 @@
 package com.turreta.config2pojo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ComTurretaConfig2pojoApplicationTests {
+	@Autowired
+	FirstConfig fc;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(fc.getAppName().equalsIgnoreCase("Config2Pojo"));
+		Assert.assertTrue(fc.getAppVersion().equalsIgnoreCase("1.0.2"));
 	}
-
 }
